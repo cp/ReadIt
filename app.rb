251d2fd3fd0f0bd18f169fb5b34b87cc02bb0732
Colby Aley <colby@aley.me>
2013-07-14 11:06:38 -0700

    Adds better error handeling and allows you to mark all posts as read

--- app.rb
+++ app.rb
@@ -6,18 +6,37 @@ get '/' do
 	redirect '/login' if session[:email].nil?
   auth = { username: session[:email], password: session[:password] }
   @posts = HTTParty.get("https://api.feedbin.me/v2/entries.json?read=false", basic_auth: auth)
-
+  redirect '/login' if @posts.code != 200
   erb :index
+end
+
+get '/read/all' do
+  auth = { username: session[:email], password: session[:password] }
+  posts = HTTParty.get("https://api.feedbin.me/v2/entries.json?read=false", basic_auth: auth)
+  to_delete = posts.map { |post| post["id"]}
+  HTTParty.post("https://api.feedbin.me/v2/unread_entries/delete.json", 
+    body: { 'unread_entries' => to_delete }.to_json, 
+    headers: { 'Content-Type' => 'application/json' },
+    basic_auth: auth)
+  if posts.code == 200
+    redirect '/'
+  else
+    "#{posts.code}! Please try again."
+  end
 end
 
 get '/read/:id' do
   redirect '/login' if session[:email].nil?
   auth = { username: session[:email], password: session[:password] }
-  HTTParty.post("https://api.feedbin.me/v2/unread_entries/delete.json", 
+  posts = HTTParty.post("https://api.feedbin.me/v2/unread_entries/delete.json", 
     body: { 'unread_entries' => params[:id] }.to_json, 
     headers: { 'Content-Type' => 'application/json' },
     basic_auth: auth)
-  redirect '/'
+  if posts.code == 200
+    redirect '/'
+  else
+    "#{posts.code}! Please try again."
+  end
 end
 
 get '/login' do
